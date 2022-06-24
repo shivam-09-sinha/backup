fc43a39b1fbeac639f05d6b01f67ad78a4164f27
shivam-09-sinha <97621729+shivam-09-sinha@users.noreply.github.com>
2022-06-24 11:59:15 +0530

    Add files via upload
    commit -2

---
+++ spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -0,0 +1,21 @@
+package com.luv2code.springdemo.rest;
+
+import com.luv2code.springdemo.entity.Customer;
+import com.luv2code.springdemo.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class CustomerRestController {
+
+	@Autowired
+	private CustomerService customerService;
+
+	@GetMapping("/customers")
+	public List<Customer> getCustomers() {
+		return customerService.getCustomers();
+	}
+}
